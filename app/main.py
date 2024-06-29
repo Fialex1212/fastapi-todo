56c56dabcdb39e387e70082b1ea3d221a2c32307
Alex <110390382+Fialex1212@users.noreply.github.com>
2024-06-29 13:10:17 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -8,19 +8,16 @@ Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-# allowed_origins = os.getenv("ALLOWED_ORIGINS", "").split(",")
+origins = [
+    "http://127.0.0.1:5501",
+]
 
-# allowed_origins = [origin.strip() for origin in allowed_origins if origin.strip()]
-
-# if not allowed_origins:
-#     raise HTTPException(status_code=500, detail="No allowed origins configured")
-
-# app.add_middleware(
-#     CORSMiddleware,
-#     allow_origins=allowed_origins,
-#     allow_credentials=True,
-#     allow_methods=["GET", "POST", "PUT", "DELETE"],
-#     allow_headers=["*"],
-# )
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "PUT", "DELETE"],
+    allow_headers=["*"],
+)
 
 app.include_router(todo.router, tags=["Todo"])
