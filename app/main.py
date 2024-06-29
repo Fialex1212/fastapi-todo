4edaa4f523076e378b57b2c54eac5bd9c0bb3159
Alex <110390382+Fialex1212@users.noreply.github.com>
2024-06-29 15:13:17 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -8,16 +8,16 @@ Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-origins = [
-    "http://127.0.0.1:5501",
-]
+# origins = [
+#     "http://127.0.0.1:5501",
+# ]
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["GET", "POST", "PUT", "DELETE"],
-    allow_headers=["*"],
-)
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=origins,
+#     allow_credentials=True,
+#     allow_methods=["GET", "POST", "PUT", "DELETE"],
+#     allow_headers=["*"],
+# )
 
 app.include_router(todo.router, tags=["Todo"])
