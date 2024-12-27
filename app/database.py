d70fface97aa441142b21c9a548723419f498c58
Aleks Seriakov <110390382+Fialex1212@users.noreply.github.com>
2024-12-27 21:05:44 +0300

    Update database.py

--- app/database.py
+++ app/database.py
@@ -7,4 +7,5 @@ SQLALCHEMY_DATABASE_URL = "sqlite:///./todo.db"
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autoflush=False, autocommit=False, bind=engine)
 
+
 Base = declarative_base()
